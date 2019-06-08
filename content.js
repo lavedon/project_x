cbf23c50048302cc8207b930106a5d76a4296684
lavedon <lavedon@gmail.com>
2019-06-08 11:23:26 -0400

    final app script

--- content.js
+++ content.js
@@ -1,62 +1,77 @@
-var scrolling = true;
-var data = [];
-var jsonData = {};
+function zip() {
+    var args = [].slice.call(arguments);
+    var longest = args.reduce(function(a,b){
+        return a.length>b.length ? a : b
+    }, []);
 
-var wait = ms => new Promise((r, j) => setTimeout(r, ms));
+    return longest.map(function(_,i){
+        return args.map(function(array){return array[i]})
+    });
+}
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Got message from background.js");
-    if (request.message == "scroll") { 
-        sendResponse({received: "Going to scroll now."});
-        console.log("got message to scroll");
-        scroll();
-    } else if (request.message == "stopScroll") {
-        sendResponse({received: "Going to stop scrolling now."});
-        console.log("message to Stop Scrolling");
-        stopScroll();
-    } else if (request.message == "grabResults") {
-        sendResponse({received: "Going to scrape data now."});
-        console.log("message to scrape data.");
-        grabResults();
-    }
 
-});
 
-async function scroll() { 
-    console.log("scroll called");
-    scrolling = true;
-    while (scrolling == true) {
-        console.log("Scrolling forever");
-        window.scrollTo(0, document.body.scrollHeight);
-        await wait(2000);
-    }
+function doPost(e) {
+// If you do not specify a sheet within the spreadsheet only a1notation will work in getRange
+var ss = SpreadsheetApp.openById("1HVbmbCTf3TtT1_PQw5vNi7lCZhRjaW9SYWn8ha4yK90");
+var sheet = ss.getSheets()[0];
+
+
+                       
+  
+var names = [];
+var facebookURLs = [];
+var splitNames = {};  
+  
+//Return if null
+if( e == undefined ) {
+  Logger.log("no data");
+  return HtmlService.createHtmlOutput("need data");
+}
+
+var data = JSON.parse(e.postData.contents);
+
+  
+var splitNames = data.split("}");
+
+Logger.log("splitNames length is: " + splitNames.length);
+
+
+var i = 0;
+  
+for(var [key,val] in splitNames){
+var entry = splitNames[key];
+var splitEntry = entry.split("facebookURL");
+var dirtyName = splitEntry[0];
+var name = dirtyName.split(":")[1].slice(1, -3);
+var facebookURL = entry.split(":")[3].slice(2, -2);
+i++;
+
+names.push(name);
+facebookURLs.push(facebookURL);
+
+// why do I have to do this?
+  if (i == splitNames.length - 1) {
+   break; 
+  }
 
 }
 
-function stopScroll() {
-    console.log("Stop scroll called");
-    scrolling = false;
-
-}
-
-async function grabResults() {
-    let block = document.getElementById("groupsMemberBrowserContent");
-    let profiles = block.getElementsByClassName("uiProfileBlockContent");
-    
-    for (let i = 0; i < profiles.length; i++) {
-        let name = profiles[i].getElementsByTagName("a")[0].innerText;
-        let dirtyFacebookURL = profiles[i].querySelector("a").getAttribute("href");
-        let facebookURL = dirtyFacebookURL.match(/.+?(?=\&)/);
-        data.push({name, facebookURL});
-    }
-
-    console.log("Data array populated.");
-    console.log(data);
-    jsonData = JSON.stringify(data);
+Logger.log("Loop finished.");
 
 
-    console.log("grabResults() called from content script.");
-    chrome.runtime.sendMessage(jsonData, 
-        (response) => response.received);
+
+var sheetArray = zip(names, facebookURLs);
+
+
+Logger.log("2d array of names are:");
+Logger.log(sheetArray);
+Logger.log("Length of sheetArray: " + sheetArray.length);
+Logger.log("Now trying to write to sheet");
+
+                               
+var range = sheet.getRange(1, 1, sheetArray.length, 2);
+ 
+range.setValues(sheetArray);
 
 }
